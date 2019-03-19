aa860e5ba340591a0b7dfbd9a0e974d1ba20b9e6
pthiel <philipp.thiel@cgc.mpg.de>
2019-03-19 11:10:53 +0100

    Some comments

--- start_container.sh
+++ start_container.sh
@@ -4,6 +4,7 @@
 # Important Settings
 #----------------------------------
 
+# Enter a valid eMail address that allows to contact the responsible colleague for this webservice
 if [ -z "$ABI_SERVICES_CONTACT_MAIL" ]
 then
   contact_email="abi-services@informatik.uni-tuebingen.de"
@@ -11,6 +12,7 @@ else
   contact_email="$ABI_SERVICES_CONTACT_MAIL"
 fi
 
+# Enter a valid URL that leads to the appropriate imprint page
 if [ -z "$ABI_SERVICES_IMPRINT_URL" ]
 then
   imprint_url="https://www-abi.informatik.uni-tuebingen.de/imprint"
@@ -18,6 +20,7 @@ else
   imprint_url="$ABI_SERVICES_IMPRINT_URL"
 fi
 
+# Enter a valid URL that leads to the appropriate GDPR declaration page
 if [ -z "$ABI_SERVICES_GDPR_URL" ]
 then
   gdpr_url="https://www-abi.informatik.uni-tuebingen.de/gdpr"
@@ -25,6 +28,7 @@ else
   gdpr_url="$ABI_SERVICES_GDPR_URL"
 fi
 
+# Here you can set an upper limit for the number of sequences allowed to be submitted
 if [ -z "$ABI_SERVICES_MULTILOC2_MAX_SEQ" ]
 then
   multiloc2_max_seq="20"
@@ -32,6 +36,7 @@ else
   multiloc2_max_seq="$ABI_SERVICES_MULTILOC2_MAX_SEQ"
 fi
 
+# Here you can specify th host port that is bound to port 80 from the container
 if [ -z "$ABI_SERVICES_MULTILOC2_PORT" ]
 then
   multiloc2_port="28020"
